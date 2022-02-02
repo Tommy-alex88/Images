4f7524fc7f34d6b3d5fa8c7efd61e3c9f7b07b14
Alexander <tommy.alex88@gmail.com>
2022-02-02 13:57:59 +0700

    feat: add axios and fetch photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "./axios";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    axios.get("/images").then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TEST APP</h1>
       </header>
     </div>
   );
